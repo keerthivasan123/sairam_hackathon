098c0b5cd953a41ab45ab87fe4a7d6e72a93689e
keerthivasan123 <keerthivasan287@gmail.com>
2019-09-20 11:35:23 +0530

    some changes

--- autism/views.py
+++ autism/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def index(request):
+    return render(request, 'autism/home.html')
+
+def predict(request):
+    return render(request, )
